918f1027a7db2c2424adbf864a93c117c6f3e54c
sceredi <ceredi.simone.iti@gmail.com>
2024-08-10 15:27:16 +0200

    chore: created mongodb models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 'use strict'
 
-export class Adder {
-  public sum(first: number, second: number): number {
-    return first + second
-  }
-}
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URI as string, {
+    socketTimeoutMS: 1000
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err))
+
+app.get('/', (_req, res) => {
+  res.send('Authentication Service')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
