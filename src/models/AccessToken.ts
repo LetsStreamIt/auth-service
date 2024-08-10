e226f4ee2c8dbbdb5ef3d1a3311910e08915cb33
sceredi <ceredi.simone.iti@gmail.com>
2024-08-10 15:27:16 +0200

    chore: created mongodb models

---
+++ src/models/AccessToken.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+export interface IAccessToken extends Document {
+  id: string
+  accessToken: string
+  userId: string
+}
+
+const AccessTokenSchema: Schema = new Schema({
+  id: { type: String, required: true, unique: true },
+  accessToken: { type: String, required: true },
+  userId: { type: String, required: true }
+})
+
+const AccessToken = mongoose.model<IAccessToken>('User', AccessTokenSchema)
+export default AccessToken
--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,9 @@
 'use strict'
 
 import { expect } from 'chai'
-import { Adder } from '../src/index'
 
-describe('An adder', () => {
-  const adder = new Adder()
-  it('should add two numbers correctly', () => {
-    expect(adder.sum(1, 2), '1 + 2 should equal 3').to.eq(3)
+describe('Useless test', () => {
+  it('adding 2 numbers work', () => {
+    expect(1 + 2, '1 + 2 should equal 3').to.eq(3)
   })
 })
