af2b099c0cd927eb900bc1d0b675e0c5048238b5
sceredi <ceredi.simone.iti@gmail.com>
2024-08-12 15:18:58 +0200

    chore: removed useless test

--- test/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import { expect } from 'chai'
-
-describe('Useless test', () => {
-  it('adding 2 numbers work', () => {
-    expect(1 + 2, '1 + 2 should equal 3').to.eq(3)
-  })
-})
