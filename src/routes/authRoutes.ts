3e31a05b5f5d3191e78bdbfdc6dbf729938fac28
sceredi <ceredi.simone.iti@gmail.com>
2024-08-16 17:30:49 +0200

    feat: added validate token api

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { registerUser, authUser } from '../controllers/authController'
-import { refreshToken } from '../controllers/tokenController'
+import { refreshToken, validateToken } from '../controllers/tokenController'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/login', authUser)
 router.post('/refresh', refreshToken)
+router.post('/validate', validateToken)
 
 export default router
