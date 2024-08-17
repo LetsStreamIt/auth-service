836aafd3ad0bfabfcd3246f8ceda4e93d4f5e164
sceredi <ceredi.simone.iti@gmail.com>
2024-08-17 10:12:49 +0200

    chore: added default mongo uri

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,9 +3,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017'
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string, {
+    await mongoose.connect(mongoUri, {
       socketTimeoutMS: 1000
     })
     console.log('MongoDB connected')
