d8443b372234634e57ae6e635b94018f942cde03
sceredi <ceredi.simone.iti@gmail.com>
2024-08-21 18:15:59 +0200

    fix: fixed cors origin settings

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true // Allow credentials (cookies, authorization headers, etc.)
 }
 
